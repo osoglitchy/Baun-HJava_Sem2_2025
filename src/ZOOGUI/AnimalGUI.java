1784c5328bf3984f5e4efe1a8054dd36222755da
Nicholas <osoglitchy@gmail.com>
2025-03-12 13:31:05 -0400

    Added Files

---
+++ src/ZOOGUI/AnimalGUI.java
@@ -0,0 +1,60 @@
+package ZOOGUI;
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class AnimalGUI{
+
+
+    public static void main(String[] args)
+    {
+        //creates window (JFrame)
+        JFrame window = new JFrame();
+        window.setSize(800 ,200);
+        window.setTitle("Philadelphia Zoo");
+        //window.setIconImage("");
+        window.getContentPane().setBackground(new Color(244, 244, 233));
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel header = new JPanel();
+        header.setSize(800, 50);
+        header.setBackground(new Color(255,255,244));
+        window.add(header);
+
+        JLabel header_icon = new JLabel(" ");
+        ImageIcon header_logo = new ImageIcon("src/ZOOGUI/zoo_blacklogo.png");
+        header_icon.setIcon(header_logo);
+        header_icon.setHorizontalAlignment(SwingConstants.CENTER);
+        header_icon.setVerticalAlignment(SwingConstants.CENTER);
+        header.add(header_icon);
+        int NumberFact = new Random().nextInt(0,11);
+        String[] foxFacts = {
+                "Foxes use the Earth's magnetic field to hunt.",
+                "Foxes can leap up to 3 meters (10 feet) high and are excellent climbers.",
+                "A fox's bushy tail helps with balance, warmth, and communication.",
+                "Foxes can adapt to almost any environment, from the Arctic tundra to urban areas.",
+                "Some foxes can produce a high-pitched 'laughing' sound when they play.",
+                "Foxes are monogamous and form lifelong bonds with their partners.",
+                "Fox kits are born blind and rely on their parents for weeks.",
+                "Foxes have exceptional hearing and can hear a mouse squeaking from 100 feet away.",
+                "Foxes communicate with a wide variety of vocalizations, from barks to screams.",
+                "Each fox has a unique scent, similar to fingerprints in humans."
+        };
+        String funfact = foxFacts[NumberFact];
+        JLabel label = new JLabel("Did you know?: " + funfact);
+        ImageIcon logo = new ImageIcon("zoo_blacklogo.png");
+        label.setIcon(logo);
+        label.setFont(new Font("Montserrat", Font.BOLD, 14));
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setVerticalAlignment(SwingConstants.CENTER);
+        label.setHorizontalTextPosition(SwingConstants.CENTER);
+        label.setVerticalTextPosition(SwingConstants.TOP);
+        window.add(label);
+
+
+
+        //set the window to visible
+        window.setVisible(true);
+
+    }
+}
